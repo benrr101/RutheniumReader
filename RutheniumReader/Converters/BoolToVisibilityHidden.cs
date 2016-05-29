b79656c65df42314038561f3ae2330fce7f6e784
Benjamin Russell <benrr101@outlook.com>
2016-05-29 15:27:15 -0700

    Stubbing out the layout

---
+++ RutheniumReader/Converters/BoolToVisibilityHidden.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace RutheniumReader.Converters
+{
+    public class BoolToVisibilityHidden : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (targetType != typeof(Visibility))
+            {
+                throw new ArgumentException("Can only convert to Visibility types");
+            } 
+
+            // Get a boolean from the object that was passed in
+            bool boolValue;
+            if (value is bool)
+            {
+                boolValue = (bool) value;
+            }
+            else
+            {
+                throw new ArgumentException("Value must be a boolean type");
+            }
+            return boolValue ? Visibility.Visible : Visibility.Hidden;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value is Visibility && (Visibility) value == Visibility.Visible;
+        }
+    }
+}
---
+++ RutheniumReader/ViewModel/BarCodeImageViewModel.cs
@@ -0,0 +1,47 @@
+﻿using System.ComponentModel;
+
+namespace RutheniumReader.ViewModel
+{
+    internal class BarCodeImageViewModel : INotifyPropertyChanged
+    {
+
+        #region UI-Bound Properties
+
+        private bool _barcodeLoaded;
+
+        public bool BarcodeLoaded
+        {
+            get { return _barcodeLoaded; }
+            set
+            {
+                _barcodeLoaded = value;
+                OnPropertyChanged("BarcodeLoaded");
+            }
+        }
+
+        private bool _imageLoaded;
+
+        public bool ImageLoaded
+        {
+            get { return _imageLoaded; }
+            set
+            {
+                _imageLoaded = value;
+                OnPropertyChanged("ImageLoaded");
+            }
+        }
+
+        #endregion
+
+        #region IPropertyChanged Interface Implementation
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string name)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
+        #endregion
+    }
+}
